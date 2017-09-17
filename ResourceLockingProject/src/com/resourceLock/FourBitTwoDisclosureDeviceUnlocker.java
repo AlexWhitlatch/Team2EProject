19cb540807b6220e4f54144a51a51e8a7c59bda7
Christopher Davis <cjd2951@gmail.com>
2017-09-17 17:04:10 -0600

    remove test output from github, add test output to git ignore, add javadoc to FBTDDU.java

--- ResourceLockingProject/src/com/resourceLock/FourBitTwoDisclosureDeviceUnlocker.java
+++ ResourceLockingProject/src/com/resourceLock/FourBitTwoDisclosureDeviceUnlocker.java
@@ -4,26 +4,40 @@
  * @version 4.1.4
  * @author Dr. Jody Paul
  * @author Stephen Bapple
+ * @author Christopher Davis
+ * @author Alex Whitlatch
+ * @author Taylor Woehrle
+ *
+ *
+ * @see <a href="http://jodypaul.com/cs/sweprin/deviceProj/projectDescription.html">Project Description</a>
+ *
  */
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
+    /* Unlock attempt limit to prevent small chance where device will not unlock. */
     private static final int LIMIT = 10;
 
-    /** Suppresses default constructor, ensuring non-instantiability. */
+    /* Suppresses default constructor, ensuring non-instantiability. */
     private FourBitTwoDisclosureDeviceUnlocker() {}
 
-    public static boolean unlock(Device key) {
+    /**
+     * Unlocks a resource controlled by a 4-bit/2-disclosure device.
+     * Behavior is unspecified if parameter is not a reference to a valid 4-bit/2-disclosure device.
+     * @param dev the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
+     * @return true if the resource is successfully unlocked (all bits are now identical); false otherwise
+     */
+    public static boolean unlock(Device dev) {
         // Check if device is already unlocked.
-        if (key.spin() == true) {
+        if (dev.spin() == true) {
             return true;
         }
 
         // Very naive solution:
         // Peek/poke the leftmost two bits to True every time.
         for (int i = 0; i < LIMIT; i++) {
-            key.peek("??--");
-            key.poke("TT--");
+            dev.peek("??--");
+            dev.poke("TT--");
 
-            if (key.spin() == true) {
+            if (dev.spin() == true) {
                 return true;
             }
         }
