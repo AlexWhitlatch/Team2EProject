0e3c6c73f2c49a1d8dbd05ee3d6d6e03b17e9543
Christopher Davis <cjd2951@gmail.com>
2017-09-17 18:55:51 -0600

    one last time

--- ResourceLockingProject/src/com/resourceLock/FourBitTwoDisclosureDeviceUnlocker.java
+++ ResourceLockingProject/src/com/resourceLock/FourBitTwoDisclosureDeviceUnlocker.java
@@ -25,8 +25,10 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker {
      * Unlocks a resource controlled by a 4-bit/2-disclosure device.
      * Behavior is unspecified if parameter is not a reference
      * to a valid 4-bit/2-disclosure device.
+     *
      * @param dev the device controlling the resource to unlock;
      *           must be a 4-bit device with 2 peek/poke bits.
+     *
      * @return true if the resource is successfully unlocked
      * (all bits are now identical); false otherwise
      */
